ef678f3042a32570977db465e123434947e243bd
Mahmoud Allam <malllam146@gmail.com>
2026-06-06 23:28:31 +0300

    Add PlanTable ,Model,DbContect and Plan Configuration

---
+++ GymMVC/Configurations/PlanConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using GymMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymMVC.Configurations
+{
+    public class PlanConfiguration : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.HasKey(p => p.Id);
+       
+            builder.Property(p => p.Name)  
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(500);
+
+            builder.Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            builder.Property(p => p.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+
+            builder.ToTable(TB =>
+            {
+                TB.HasCheckConstraint("PlanDurationCheck", "DurationDays Between 1 AND 365");
+            });
+
+        }
+    }
+}
---
+++ GymMVC/DBContext/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GymMVC.Configurations;
+using GymMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymMVC.DBContext
+{
+    public class GymDbContext : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Database=GymDb; Trusted_Connection=true; TrustServerCertificate=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PlanConfiguration());
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+
+    }
+}
